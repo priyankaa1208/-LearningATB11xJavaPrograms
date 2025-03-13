e25986a0a9da740488b275cd4c9ce45465e9bdab
priyakoti <priyankamaramreddy@gmail.com>
2025-03-13 22:02:04 +0530

    It will print my name

--- src/JavaPackage/HelloWorld.java
+++ src/JavaPackage/HelloWorld.java
@@ -3,5 +3,6 @@ package JavaPackage;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("This is koti");
     }
 }
